cff043e831a3191c59a3f2dfba7066b5fc6224b5
sanaa <Sanaakumar07@gmail.com>
2025-08-30 11:38:49 +0530

    Initial Commit

---
+++ fibonacci/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  fibonacci
+//
+//  Created by Sanaa Kumar on 30/08/25.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
